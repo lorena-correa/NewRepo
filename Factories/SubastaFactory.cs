47d94ed6442f11e5ac567e7389730edf7a04ebe1
leidy-correa <lorena99lozano@email.com>
2026-04-11 02:19:05 -0500

    Se añade una nueva clase

---
+++ Factories/SubastaFactory.cs
@@ -0,0 +1,21 @@
+﻿public class SubastaFactory
+{
+    public static Subasta Crear(SubastaDTO dto)
+    {
+        SubastaStrategy estrategia = dto.TipoSubasta switch
+        {
+            "Ascendente" => new SubastaAscendente(),
+            "Descendente" => new SubastaDescendente(),
+            "Cerrada" => new SubastaCerrada(),
+            _ => throw new Exception("Tipo inválido")
+        };
+
+        return new Subasta
+        {
+            NombreProducto = dto.NombreProducto,
+            PrecioActual = dto.PrecioActual,
+            Activa = dto.Activa,
+            Estrategia = estrategia
+        };
+    }
+}
